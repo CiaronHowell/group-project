374b520b0008d04162e318baecb28d80a94e85b9
iAmPazuzu <johnzkbanda@gmail.com>
2019-04-29 10:57:28 +0100

    updated sql, model and SQL commands on text file

--- database/mwbsql.sql
+++ database/mwbsql.sql
@@ -149,11 +149,6 @@ CREATE TABLE IF NOT EXISTS `Yum`.`recipes_under_total_time` (`id` INT);
 CREATE TABLE IF NOT EXISTS `Yum`.`admin_details` (`id` INT);
 
 -- -----------------------------------------------------
--- Placeholder table for view `Yum`.`recipes_under_certain_calories`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Yum`.`recipes_under_certain_calories` (`id` INT);
-
--- -----------------------------------------------------
 -- Placeholder table for view `Yum`.`recipe_rating_under`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Yum`.`recipe_rating_under` (`id` INT);
@@ -169,19 +164,14 @@ CREATE TABLE IF NOT EXISTS `Yum`.`recipe_under_prep_time` (`id` INT);
 CREATE TABLE IF NOT EXISTS `Yum`.`recipe_under_cooking_time` (`id` INT);
 
 -- -----------------------------------------------------
--- Placeholder table for view `Yum`.`users_inventory`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Yum`.`users_inventory` (`id` INT);
-
--- -----------------------------------------------------
 -- Placeholder table for view `Yum`.`recipes_with_certain_ingredients`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Yum`.`recipes_with_certain_ingredients` (`id` INT);
+CREATE TABLE IF NOT EXISTS `Yum`.`recipes_with_certain_ingredients` (`idRecipe` INT, `Recipe_Name` INT, `Cooking_Time` INT, `Prep_Time` INT, `Total_Time` INT, `Calories` INT, `Rating` INT, `Instructions` INT, `Review` INT, `Dietry_Information` INT);
 
 -- -----------------------------------------------------
 -- Placeholder table for view `Yum`.`users_saved_recipes`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Yum`.`users_saved_recipes` (`id` INT);
+CREATE TABLE IF NOT EXISTS `Yum`.`users_saved_recipes` (`idRecipe` INT, `Recipe_Name` INT, `Cooking_Time` INT, `Prep_Time` INT, `Total_Time` INT, `Calories` INT, `Rating` INT, `Instructions` INT, `Review` INT, `Dietry_Information` INT);
 
 -- -----------------------------------------------------
 -- Placeholder table for view `Yum`.`user_forgotten_username_password`
@@ -192,6 +182,26 @@ CREATE TABLE IF NOT EXISTS `Yum`.`user_forgotten_username_password` (`Username` 
 -- Placeholder table for view `Yum`.`admin_forgotten_username_passowrd`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Yum`.`admin_forgotten_username_passowrd` (`Username` INT, `User_Password` INT);
+
+-- -----------------------------------------------------
+-- Placeholder table for view `Yum`.`users_inventory_new`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Yum`.`users_inventory_new` (`idIngredients` INT, `Ingredient_Name` INT, `Food_Group` INT);
+
+-- -----------------------------------------------------
+-- Placeholder table for view `Yum`.`ingredients_within_certain_food_group`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Yum`.`ingredients_within_certain_food_group` (`idIngredients` INT, `Ingredient_Name` INT, `Food_Group` INT);
+
+-- -----------------------------------------------------
+-- Placeholder table for view `Yum`.`recipes_under_certain_calories`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Yum`.`recipes_under_certain_calories` (`id` INT);
+
+-- -----------------------------------------------------
+-- Placeholder table for view `Yum`.`recipes_with_certain_dietry_information`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Yum`.`recipes_with_certain_dietry_information` (`idRecipe` INT, `Recipe_Name` INT, `Cooking_Time` INT, `Prep_Time` INT, `Total_Time` INT, `Calories` INT, `Rating` INT, `Instructions` INT, `Review` INT, `Dietry_Information` INT);
 
 -- -----------------------------------------------------
 -- View `Yum`.`ingredient_details`
@@ -234,14 +244,6 @@ CREATE OR REPLACE VIEW `admin_details` AS
 SELECT * FROM yum.user WHERE isAdmin = true;
 
 -- -----------------------------------------------------
--- View `Yum`.`recipes_under_certain_calories`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `Yum`.`recipes_under_certain_calories`;
-USE `Yum`;
-CREATE  OR REPLACE VIEW `recipes_under_certain_calories` AS
-SELECT * FROM yum.recipe WHERE recipe.calories < 50;
-
--- -----------------------------------------------------
 -- View `Yum`.`recipe_rating_under`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `Yum`.`recipe_rating_under`;
@@ -266,34 +268,60 @@ CREATE  OR REPLACE VIEW `recipe_under_cooking_time` AS
 SELECT * FROM yum.recipe WHERE Cooking_Time < 90;
 
 -- -----------------------------------------------------
--- View `Yum`.`users_inventory`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `Yum`.`users_inventory`;
-USE `Yum`;
-CREATE  OR REPLACE VIEW `users_inventory` AS
-SELECT ingredients.*
-FROM yum.ingredients INNER JOIN yum.user ON ingredients.idIngredients = yum.user.idUser
-WHERE yum.user.idUser = 1;
-
--- -----------------------------------------------------
 -- View `Yum`.`recipes_with_certain_ingredients`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `Yum`.`recipes_with_certain_ingredients`;
 USE `Yum`;
-CREATE  OR REPLACE VIEW `recipes_with_certain_ingredients` AS
-SELECT recipe.*
-FROM Recipe INNER JOIN ingredients ON recipe.idRecipe = ingredients.idIngredients
-WHERE ingredients.Ingredient_Name = "Chicken";
+CREATE 
+     OR REPLACE ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `recipes_with_certain_ingredients` AS
+    SELECT 
+        `recipe`.`idRecipe` AS `idRecipe`,
+        `recipe`.`Recipe_Name` AS `Recipe_Name`,
+        `recipe`.`Cooking_Time` AS `Cooking_Time`,
+        `recipe`.`Prep_Time` AS `Prep_Time`,
+        `recipe`.`Total_Time` AS `Total_Time`,
+        `recipe`.`Calories` AS `Calories`,
+        `recipe`.`Rating` AS `Rating`,
+        `recipe`.`Instructions` AS `Instructions`,
+        `recipe`.`Review` AS `Review`,
+        `recipe`.`Dietry_Information` AS `Dietry_Information`
+    FROM
+        ((`recipe`
+        JOIN `ingredients_needed` ON ((`recipe`.`idRecipe` = `ingredients_needed`.`idRec`)))
+        JOIN `ingredients` ON ((`ingredients_needed`.`idIngredients` = `ingredients`.`idIngredients`)))
+    WHERE
+        (`ingredients`.`Ingredient_Name` = 'Chicken Breast');
 
 -- -----------------------------------------------------
 -- View `Yum`.`users_saved_recipes`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `Yum`.`users_saved_recipes`;
 USE `Yum`;
-CREATE  OR REPLACE VIEW `users_saved_recipes` AS
-SELECT recipe.*
-FROM recipe INNER JOIN yum.user ON yum.recipe.idRecipe = yum.user.idUser
-WHERE idUser = 1;
+CREATE 
+     OR REPLACE ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `users_saved_recipes` AS
+    SELECT DISTINCT
+        `recipe`.`idRecipe` AS `idRecipe`,
+        `recipe`.`Recipe_Name` AS `Recipe_Name`,
+        `recipe`.`Cooking_Time` AS `Cooking_Time`,
+        `recipe`.`Prep_Time` AS `Prep_Time`,
+        `recipe`.`Total_Time` AS `Total_Time`,
+        `recipe`.`Calories` AS `Calories`,
+        `recipe`.`Rating` AS `Rating`,
+        `recipe`.`Instructions` AS `Instructions`,
+        `recipe`.`Review` AS `Review`,
+        `recipe`.`Dietry_Information` AS `Dietry_Information`
+    FROM
+        ((`recipe`
+        JOIN `saved_recipes` ON ((`recipe`.`idRecipe` = `saved_recipes`.`idRecipe`)))
+        JOIN `user` ON ((`saved_recipes`.`idUser` = `user`.`idUser`)))
+    WHERE
+        (`user`.`idUser` = 1);
 
 -- -----------------------------------------------------
 -- View `Yum`.`user_forgotten_username_password`
@@ -314,6 +342,89 @@ CREATE  OR REPLACE VIEW `admin_forgotten_username_passowrd` AS
 SELECT Username, User_Password
 FROM yum.user
 WHERE isAdmin = true;
+
+-- -----------------------------------------------------
+-- View `Yum`.`users_inventory_new`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Yum`.`users_inventory_new`;
+USE `Yum`;
+CREATE 
+     OR REPLACE ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `users_inventory_new` AS
+    SELECT DISTINCT
+        `ingredients`.`idIngredients` AS `idIngredients`,
+        `ingredients`.`Ingredient_Name` AS `Ingredient_Name`,
+        `ingredients`.`Food_Group` AS `Food_Group`
+    FROM
+        ((`ingredients`
+        JOIN `inventory` ON ((`ingredients`.`idIngredients` = `inventory`.`idIngredient`)))
+        JOIN `user` ON ((`user`.`idUser` = `inventory`.`idUse`)))
+    WHERE
+        (`user`.`idUser` = 2);
+
+-- -----------------------------------------------------
+-- View `Yum`.`ingredients_within_certain_food_group`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Yum`.`ingredients_within_certain_food_group`;
+USE `Yum`;
+CREATE 
+     OR REPLACE ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `ingredients_within_certain_food_group` AS
+    SELECT 
+        `ingredients`.`idIngredients` AS `idIngredients`,
+        `ingredients`.`Ingredient_Name` AS `Ingredient_Name`,
+        `ingredients`.`Food_Group` AS `Food_Group`
+    FROM
+        `ingredients`
+    WHERE
+        (`ingredients`.`Food_Group` = 'Carbohydrates');
+
+-- -----------------------------------------------------
+-- View `Yum`.`recipes_under_certain_calories`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Yum`.`recipes_under_certain_calories`;
+USE `Yum`;
+CREATE 
+     OR REPLACE ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `recipes_under_certain_calories` AS
+    SELECT 
+        *
+    FROM
+        `recipe`
+    WHERE
+        (`recipe`.`Calories` < 2000);
+
+-- -----------------------------------------------------
+-- View `Yum`.`recipes_with_certain_dietry_information`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Yum`.`recipes_with_certain_dietry_information`;
+USE `Yum`;
+CREATE 
+     OR REPLACE ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `recipes_with_certain_dietry_information` AS
+    SELECT 
+        `recipe`.`idRecipe` AS `idRecipe`,
+        `recipe`.`Recipe_Name` AS `Recipe_Name`,
+        `recipe`.`Cooking_Time` AS `Cooking_Time`,
+        `recipe`.`Prep_Time` AS `Prep_Time`,
+        `recipe`.`Total_Time` AS `Total_Time`,
+        `recipe`.`Calories` AS `Calories`,
+        `recipe`.`Rating` AS `Rating`,
+        `recipe`.`Instructions` AS `Instructions`,
+        `recipe`.`Review` AS `Review`,
+        `recipe`.`Dietry_Information` AS `Dietry_Information`
+    FROM
+        `recipe`
+    WHERE
+        (`recipe`.`Dietry_Information` = 'Milk');
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
